b8d228a99ef08c3339a31912a0e4819ad0fe1971
SiangWei91 <siangwei91@gmail.com>
2024-12-26 09:49:58 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'B15-Stock-Take-cache-v2.0';
+const urlsToCache = [
+    '/B15-Stock-Take/',
+    '/B15-Stock-Take/index.html',
+    '/B15-Stock-Take/app.js',
+    '/B15-Stock-Take/manifest.json',
+    '/B15-Stock-Take/icons/CR1icon-192x192.png',
+    '/B15-Stock-Take/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
